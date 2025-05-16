442fb84ff33f4d84254ac9b52c9add3a608ec0e0
Juan Camilo Parra Ospina <parraospinajuancamilo@gmail.com>
2025-05-16 16:41:48 -0500

    obtener nombre terminado

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const pool = require('./db');
+const bcrypt = require('bcrypt');
 
 
 const { enviarOTP, verificarOTP } = require('./otpService');
@@ -12,6 +14,11 @@ const { verificarToken } = require('./middleware');
 
 const app = express();
 const port = 3000;
+
+app.use(cors({
+  origin: 'http://localhost:5173', // o el dominio del frontend si está desplegado
+  credentials: true
+}));
 
 app.use(cors());
 app.use(express.json());
@@ -33,6 +40,23 @@ app.post('/solicitar-otp', async (req, res) => {
     res.status(500).json({ mensaje: 'Error enviando OTP' });
   }
 });
+
+app.post('/reenviar-otp', async (req, res) => {
+  const { correo } = req.body;
+
+  if (!correo) {
+    return res.status(400).json({ error: 'El correo es obligatorio.' });
+  }
+
+  try {
+    await enviarOTP(correo);
+    res.status(200).json({ message: 'Código reenviado con éxito.' });
+  } catch (error) {
+    console.error('Error al reenviar OTP:', error);
+    res.status(500).json({ error: 'Error al reenviar el código.' });
+  }
+});
+
 
 
 app.post('/verificar-otp', async (req, res) => {
@@ -92,6 +116,37 @@ app.post('/login', async (req, res) => {
   } catch (error) {
     console.error('Error en login:', error);
     res.status(401).json({ mensaje: error.message });
+  }
+});
+
+app.post('/obtener-nombre', async (req, res) => {
+  const { correo, contrasena } = req.body;
+
+  if (!correo || !contrasena) {
+    return res.status(400).json({ mensaje: 'Correo y contraseña son obligatorios' });
+  }
+
+  try {
+    const [rows] = await pool.execute(
+      'SELECT * FROM USUARIO WHERE correo = ?',
+      [correo]
+    );
+
+    if (rows.length === 0) {
+      return res.status(401).json({ mensaje: 'Usuario no encontrado' });
+    }
+
+    const usuario = rows[0];
+
+    const esValida = await bcrypt.compare(contrasena, usuario.contrasena);
+    if (!esValida) {
+      return res.status(401).json({ mensaje: 'Contraseña incorrecta' });
+    }
+
+    res.json({ nombre: usuario.nombre });
+  } catch (error) {
+    console.error('Error en /obtener-nombre:', error);
+    res.status(500).json({ mensaje: 'Error en el servidor' });
   }
 });
 
